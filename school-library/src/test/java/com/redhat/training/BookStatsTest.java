57be605c484c669744ced5eb18cf8e06dbaf4c77
ryu-shiozawa <ryu.shiozawa.sf@nttcom.co.jp>
2024-03-07 00:57:21 -0500

    Refactor

--- school-library/src/test/java/com/redhat/training/BookStatsTest.java
+++ school-library/src/test/java/com/redhat/training/BookStatsTest.java
@@ -11,17 +11,24 @@ public class BookStatsTest {
     @Test
     public void countingWordsOfEmptyBookReturnsZero() {
         // Given
-        Book book = new Book("someISBN");
+        Book book = new Book("someISBN", "this is the content");
 
         // When
         double wordCount = BookStats.countWords(book);
 
         // Then
-        assertEquals(0, wordCount);
+        assertEquals(4, wordCount);
     }
 
     @Test
     public void countingWordsReturnsNumberOfWordsInBook() {
-        assertEquals(0, 1); // Replace this line with the actual test code...
+        // Given
+        Book book = new Book("someISBN", "this is the content");
+
+        // When
+        double wordCount = BookStats.countWords(book);
+
+        // Then
+        assertEquals(4, wordCount);
     }
 }
--- shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
+++ shipping-calculator/src/test/java/com/redhat/shipping/ShippingCalculatorTest.java
@@ -4,7 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class ShippingCalculatorTest {
 
     // @todo: add tests
+    @Test
+    public void canCalculateTheCostForARegion() {
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        assertEquals(0, calculator.costForRegion("A Region"));
+    }
+    public void onNARegionTheCostIs100() {
+        // Given a shipping calculator
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        // When NA is the country region
+        int calculatedCost = calculator.costForRegion("NA");
+
+        // Then the shipping cost is 100
+        assertEquals(100, calculatedCost);
+    }
+    public void onAPACRegionTheCostIs400() {
+        // Given a shipping calculator
+        ShippingCalculator calculator = new ShippingCalculator();
+
+        // When APAC is the country region
+        int calculatedCost = calculator.costForRegion("APAC");
+
+        // Then the shipping cost is 400
+        assertEquals(400, calculatedCost);
+    }
 }
